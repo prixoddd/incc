70e303dc3da5f4435edfb8aa05ecab307b8f70d7
Nikita <nikita.derjugin@gmail.com>
2023-01-28 20:05:47 +0200

    12th commit

---
+++ t12.js
@@ -0,0 +1,9 @@
+function simpleMultiplication(number) {
+    //     number % 2 ? number * 8 : number * 9
+      if (number % 2 == 0) {
+        return number * 8;
+      } else {
+        return number * 9;
+      }
+    }
+    
