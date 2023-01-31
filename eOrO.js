205dbaa16f727b58041b3f6075733896c9435a8e
Nikita <nikita.derjugin@gmail.com>
2023-02-01 00:24:21 +0200

    1.02 2 done

--- eOrO.js
+++ eOrO.js
@@ -7,3 +7,12 @@ function evenOrOdd(number) {
   }
 
 console.log(evenOrOdd(-42));
+
+function removeEveryOther(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i += 2) {
+      res.push(arr[i]);
+    }
+    return res;
+  }
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
