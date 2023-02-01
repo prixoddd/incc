fde169a65e5d1e500c70597aa9d67bd22dcdb32f
Nikita <nikita.derjugin@gmail.com>
2023-02-01 20:50:36 +0200

    1.02 8 done

---
+++ t22.js
@@ -0,0 +1,20 @@
+function countPositivesSumNegatives(input) {
+    const answer = []
+    let positiveSum = 0
+    let negativeSum = 0
+
+    if(input && input.length) {
+      for(let i = 0; i < input.length; i++) {
+        if(input[i] > 0) {
+            positiveSum += 1
+        } else {
+            negativeSum += input[i]
+        }
+      }
+      answer.push(positiveSum)
+      answer.push(negativeSum)
+    }
+    return answer
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
