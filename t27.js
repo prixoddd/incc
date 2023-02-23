3ae6dbad8bb9cccf0f8b37b4fd6ded87886fe696
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-23 21:06:27 +0200

    1

--- t27.js
+++ t27.js
@@ -15,3 +15,12 @@
 // }
 //
 // console.log(mixedNumbers(['IV', 5, 'six']));
+
+// function popShift(arr) {
+//     let popped = arr.pop();
+//     let shifted = arr.shift();
+//     return [shifted, popped];
+// }
+//
+// // do not change code below this line
+// console.log(popShift(["challenge", "is", "not", "complete"]));
