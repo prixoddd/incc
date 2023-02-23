26d6007457912b849070cd52e23393c56e8bc618
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-23 20:56:39 +0200

    1

--- t27.js
+++ t27.js
@@ -1,1 +1,8 @@
-let yourArray = ["a", 2, true, "c", null, { name: "john" }];
+// let yourArray = ["a", 2, true, "c", null, { name: "john" }];
+
+// let myArray = ["a", "b", "c", "d"];
+// // Only change code below this line
+// myArray[1] = "not b anymore";
+// // Only change code above this line
+// console.log(myArray);
+
