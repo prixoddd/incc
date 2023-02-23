b88b25c35ce5604296e5763b676a5eb7e785819c
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-23 21:35:51 +0200

    1

---
+++ t28.js
@@ -0,0 +1,12 @@
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+
+// var arrLength = [];
+// for (var i = 0; i < arr.length; i++) {
+//     arrLength[i] = arr[i].length;
+// }
+
+var arrLength = arr.map(function(name) {
+    return name.length;
+});
+
+console.log( arrLength ); // 4,5,2,5
