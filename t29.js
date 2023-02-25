f195914ff972be3ca8fac511867ae12d4168f27d
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-25 19:48:34 +0200

    1

---
+++ t29.js
@@ -0,0 +1,18 @@
+<input id="search"/>
+2<button id="find">find</button>
+3
+4<script>
+5    const searchInputElement = document.getElementById('search')
+6    const huntButtonElement = document.getElementById('find')
+7
+8    XXX.addEventListener(YYY, () => {
+    9        console.log(searchInputElement.value)
+    10    })
+11
+12/*
+13Что нужно написать внутри скобок вместо XXX и YYY, чтобы при нажатии на кнопку увидеть текст, введённый в input?
+14Ответ дайте через пробел.
+15Пример ответа: firstword secondword
+16*/
+17
+18</script>
