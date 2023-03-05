cf7be8aec042f38746642648a91e62e79132945c
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-03-05 22:58:54 +0200

    done 3

--- 30.js
+++ 30.js
@@ -18,3 +18,26 @@
 // }
 //
 // console.log(greet('Daniel', 'Daniel'));
+
+function XO(str) {
+    let b = str.toLowerCase().split("")
+    let result = {};
+    for (let i = 0; i < b.length; ++i)
+    {
+        let a = b[i];
+        if (result[a] !== undefined)
+            ++result[a];
+        else
+            result[a] = 1;
+    }
+    return result.x === result.o;
+
+}
+
+console.log(XO("xxOo"));
+//
+// function XO(str) {
+//     let x = str.match(/x/gi);
+//     let o = str.match(/o/gi);
+//     return (x && x.length) === (o && o.length);
+// }
