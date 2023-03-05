6a70ba24b607717d7255864eb64f79b02c59848d
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-03-05 22:44:10 +0200

    done 2

--- 30.js
+++ 30.js
@@ -1,10 +1,20 @@
-function check(a, x) {
-    for(let i = 0; i < a.length; i++) {
-        if (a[i] === x) {
-            return true;
-        }
-    }
-    return false;
-}
+// function check(a, x) {
+//     for(let i = 0; i < a.length; i++) {
+//         if (a[i] === x) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+//
+// console.log(check([66, 101], 77));
 
-console.log(check([66, 101], 77));
+// function greet (name, owner) {
+//     if (name === owner) {
+//         return `Hello boss`;
+//     } else {
+//         return "Hello guest";
+//     }
+// }
+//
+// console.log(greet('Daniel', 'Daniel'));
